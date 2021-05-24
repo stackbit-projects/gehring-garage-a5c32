9959a89e7c2b3222eb8eed1e2f1988380c8fcbb2
Stackbit Code Editor <projects@stackbit.com>
2021-05-24 10:22:57 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -21,15 +21,15 @@ export default class Body extends React.Component {
                     <meta name="google" content="notranslate" />
                     <script type="application/ld+json">
                     {
-                            "@context": "https://schema.org",
-                            "@type": ["TouristAttraction", "AmusementPark"],
-                            "name": "Disneyland Paris",
-                            "description": "It's an amusement park in Marne-la-Vallée, near Paris, in France and is the most visited theme park in all of France and Europe.",
-                            "openingHours":["Mo-Fr 10:00-19:00", "Sa 10:00-22:00", "Su 10:00-21:00"],
-                            "isAccessibleForFree": false,
-                            "currenciesAccepted": "EUR",
-                            "paymentAccepted":"Cash, Credit Card",
-                            "url":"http://www.disneylandparis.it/"
+                        "@context": "https://schema.org",
+                        "@type": ["TouristAttraction", "AmusementPark"],
+                        "name": "Disneyland Paris",
+                        "description": "It's an amusement park in Marne-la-Vallée, near Paris, in France and is the most visited theme park in all of France and Europe.",
+                        "openingHours":["Mo-Fr 10:00-19:00", "Sa 10:00-22:00", "Su 10:00-21:00"],
+                        "isAccessibleForFree": false,
+                        "currenciesAccepted": "EUR",
+                        "paymentAccepted":"Cash, Credit Card",
+                        "url":"http://www.disneylandparis.it/"
                     }
                     </script>
                     <meta name="description" content={_.get(this.props, 'pageContext.frontmatter.seo.description', null) || ''} />
