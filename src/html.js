1e036edd1638ca7be1f9d14601f2cbaf4bb032e5
Stackbit Code Editor <projects@stackbit.com>
2021-05-24 11:04:24 +0000

    html.js: changed

--- src/html.js
+++ src/html.js
@@ -6,30 +6,6 @@ export default function HTML(props) {
     <html {...props.htmlAttributes}>
       <head>
         {props.headComponents}
-        <script type="application/ld+json">
-        {
-            "@context": "https://schema.org",
-            "@type": ["AutoRepairtion", "AutoBodyShop"],
-            "name": "Gehring Garage",
-            "address": {
-                "@type": "PostalAddress",
-                "addressCountry": "Germany",
-                "addressLocality": "Neuhausen",
-                "addressRegion": "BW",
-                "postalCode": "75242",
-                "streetAddress": "Pforzheimer Straße 9"
-            },
-            "description": "Freie KFZ Werkstatt in Neuhausen (Enzkreis) für alle Marken. Reifenwechsel, Inspektion, Smart repair, Restauration und vieles mehr.",
-            "openingHours":["Mo-Fr 10:00-18:00", "Sa 11:00-16:00"],
-            "currenciesAccepted": "EUR",
-            "priceRange": "$",
-            "paymentAccepted":"Cash, Credit Card, Bank transfer",
-            "legalName": "Autowerkstatt Uwe Gehring",
-            "tel": "+4972346921",
-            "email": "info@gehring-garage.de",
-            "url":"http://www.gehring-garage.de/"
-        }
-        </script>
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
